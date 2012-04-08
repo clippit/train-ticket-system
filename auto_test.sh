6768555e7dbfe4bd9854c545b06c7397ee0fe67d
Clippit <zlthooray@gmail.com>
2012-04-09 00:26:30 +0800

    add concurrency test script

---
+++ auto_test.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+data_file="data.sqlite"
+sql_file="dummy.sql"
+
+# init database
+if [[ -e $data_file ]]; then
+	rm -f $data_file
+fi
+sqlite3 $data_file < $sql_file
+
+# init commands
+params=("-u user1 -p 123456"
+	"-u user1 -p 123456 -s"
+	"-u user1 -p 123456 -s --from nanjing"
+	"-u user2 -p 123456 -o --number G1 --amount 5"
+	"-u user1 -p 123456 -s --to shanghai"
+	"-u user2 -p 123456 -s --number 1 --from nanjing"
+	"-u user1 -p 123456 -o --number G1 --amount 5 -v"
+	"-u user2 -p 123456 -s --to beijing -v"
+	"-u user1 -p 123456 -o --number G1 --amount 5"
+	"-u user2 -p 123456 -s --from shanghai"
+	"-u user2 -p 123456 -o --number G7001 --amount 3 -v"
+	"-u user2 -p 123456 -v -f --orderid 1"
+	"-u user1 -p 123456 -s --to shanghai --from beijing"
+	"-u user1 -p 123456 -f --orderid 1"
+	"-u user1 -p 123456 -o --number G1 --amount 5 -v"
+)
+
+# start
+killall ticketd
+./ticketd
+for p in "${params[@]}"; do
+	./ticket $p &
+done
+
+wait
+killall ticketd
+exit
